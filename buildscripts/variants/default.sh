5b17f7b8d37ee7ce95c053ae387300d37a858289
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-07-17 23:40:04 +0530

    feat: default & full build(s)

---
+++ buildscripts/variants/default.sh
@@ -0,0 +1,181 @@
+#!/bin/bash -e
+
+. ../../include/depinfo.sh
+. ../../include/path.sh
+
+if [ "$1" == "build" ]; then
+	true
+elif [ "$1" == "clean" ]; then
+	rm -rf _build$ndk_suffix
+	exit 0
+else
+	exit 255
+fi
+
+mkdir -p _build$ndk_suffix
+cd _build$ndk_suffix
+
+cpu=armv7-a
+[[ "$ndk_triple" == "aarch64"* ]] && cpu=armv8-a
+[[ "$ndk_triple" == "x86_64"* ]] && cpu=generic
+[[ "$ndk_triple" == "i686"* ]] && cpu="i686 --disable-asm"
+
+cpuflags=
+[[ "$ndk_triple" == "arm"* ]] && cpuflags="$cpuflags -mfpu=neon -mcpu=cortex-a8"
+
+../configure \
+	--target-os=android --enable-cross-compile --cross-prefix=$ndk_triple- --cc=$CC \
+	--arch=${ndk_triple%%-*} --cpu=$cpu --pkg-config=pkg-config \
+	--extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib" \
+	\
+	--disable-gpl \
+	--disable-nonfree \
+	--enable-version3 \
+	--disable-static \
+	--disable-vulkan \
+	--disable-iconv \
+	\
+	--disable-muxers \
+	--disable-decoders \
+	--disable-encoders \
+	--disable-demuxers \
+	--disable-parsers \
+	--disable-protocols \
+	--disable-devices \
+	--disable-filters \
+	--disable-doc \
+	--disable-avdevice \
+	--disable-postproc \
+	--disable-programs \
+	--disable-gray \
+	--disable-swscale-alpha \
+	\
+	--enable-jni \
+	--disable-bsfs \
+	--disable-stripping \
+	--disable-mediacodec \
+	\
+	--disable-dxva2 \
+	--disable-vaapi \
+	--disable-vdpau \
+	--disable-bzlib \
+	--disable-linux-perf \
+	--disable-videotoolbox \
+	--disable-audiotoolbox \
+	\
+	--enable-small \
+	--enable-shared \
+	--enable-hwaccels \
+	--enable-optimizations \
+	--enable-runtime-cpudetect \
+	\
+	--enable-mbedtls \
+	\
+	--enable-libdav1d \
+	\
+	--enable-avutil \
+	--enable-avcodec \
+	--enable-avfilter \
+	--enable-avformat \
+	--enable-swscale \
+	--enable-swresample \
+	\
+	--enable-decoder=aac* \
+	--enable-decoder=ac3 \
+	--enable-decoder=alac \
+	--enable-decoder=als \
+	--enable-decoder=ape \
+	--enable-decoder=atrac* \
+	--enable-decoder=eac3 \
+	--enable-decoder=flac \
+	--enable-decoder=gsm* \
+	--enable-decoder=mp1* \
+	--enable-decoder=mp2* \
+	--enable-decoder=mp3* \
+	--enable-decoder=mpc* \
+	--enable-decoder=opus \
+	--enable-decoder=ra* \
+	--enable-decoder=ralf \
+	--enable-decoder=shorten \
+	--enable-decoder=tak \
+	--enable-decoder=tta \
+	--enable-decoder=vorbis \
+	--enable-decoder=wavpack \
+	--enable-decoder=wma* \
+	--enable-decoder=pcm* \
+	--enable-decoder=dsd* \
+	\
+	--enable-demuxer=aac \
+	--enable-demuxer=ac3 \
+	--enable-demuxer=aiff \
+	--enable-demuxer=ape \
+	--enable-demuxer=asf \
+	--enable-demuxer=au \
+	--enable-demuxer=avi \
+	--enable-demuxer=flac \
+	--enable-demuxer=flv \
+	--enable-demuxer=matroska \
+	--enable-demuxer=mov \
+	--enable-demuxer=m4v \
+	--enable-demuxer=mp3 \
+	--enable-demuxer=mpc* \
+	--enable-demuxer=ogg \
+	--enable-demuxer=pcm* \
+	--enable-demuxer=rm \
+	--enable-demuxer=shorten \
+	--enable-demuxer=tak \
+	--enable-demuxer=tta \
+	--enable-demuxer=wav \
+	--enable-demuxer=wv \
+	--enable-demuxer=xwma \
+	--enable-demuxer=dsf \
+	--enable-demuxer=dts \
+	--enable-demuxer=truehd \
+	\
+	--enable-parser=aac* \
+	--enable-parser=ac3 \
+	--enable-parser=cook \
+	--enable-parser=dca \
+	--enable-parser=flac \
+	--enable-parser=gsm \
+	--enable-parser=mpegaudio \
+	--enable-parser=tak \
+	--enable-parser=vorbis \
+	\
+	--enable-filter=overlay \
+	--enable-filter=equalizer \
+	\
+	--enable-protocol=async \
+	--enable-protocol=cache \
+	--enable-protocol=data \
+	--enable-protocol=ffrtmphttp \
+	--enable-protocol=file \
+	--enable-protocol=ftp \
+	--enable-protocol=hls \
+	--enable-protocol=http \
+	--enable-protocol=httpproxy \
+	--enable-protocol=https \
+	--enable-protocol=pipe \
+	--enable-protocol=rtmp \
+	--enable-protocol=rtmps \
+	--enable-protocol=rtmpt \
+	--enable-protocol=rtmpts \
+	--enable-protocol=rtp \
+	--enable-protocol=subfile \
+	--enable-protocol=tcp \
+	--enable-protocol=tls \
+	--enable-protocol=srt \
+	\
+	--enable-network \
+
+make -j$cores
+make DESTDIR="$prefix_dir" install
+
+ln -sf "$prefix_dir"/lib/libswresample.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libpostproc.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libavutil.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libavcodec.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libavformat.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libswscale.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libavfilter.so "$native_dir"
+ln -sf "$prefix_dir"/lib/libavdevice.so "$native_dir"
